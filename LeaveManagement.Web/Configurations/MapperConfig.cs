a966a27a80cdae8b875e59c5c7551d4e7009a01e
Piotr Bąk <piotr.andrzej.bak@outlook.com>
2022-09-25 23:17:24 +0200

    Modified the crud screens and added datatables and sqal. End of Leave Type Managament Module.

---
+++ LeaveManagement.Web/Configurations/MapperConfig.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using LeaveManagement.Web.Data;
+using LeaveManagement.Web.Models;
+
+namespace LeaveManagement.Web.Configurations
+{
+    public class MapperConfig : Profile
+    {
+        public MapperConfig()
+        {
+            // define route to mak conversion from one to another and back
+
+            CreateMap<LeaveType, LeaveTypeVM>().ReverseMap();
+        }
+    }
+}
---
+++ LeaveManagement.Web/Models/LeaveTypeVM.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LeaveManagement.Web.Models
+{
+    public class LeaveTypeVM
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Leave Type Name")]
+        [Required(ErrorMessage = "Nazwa jest wymagana")]
+        public string Name { get; set; }
+
+        [Display(Name = "Default Number of Days")]
+        [Required(ErrorMessage = "Wartość jest wymagana")]
+        [Range(1, 25, ErrorMessage = "Proszę podać prawidłową wartość")]
+        public int? DefaultDays { get; set; }
+    }
+}
